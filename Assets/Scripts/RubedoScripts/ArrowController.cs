f560c81eb14cfc609cb0fec15c466aed8c2222bc
Roxana Ciucioiu <roxana-denisa.ciucioiu@student.upt.ro>
2024-03-10 19:39:55 +0200

    Some Rubedo Updates

--- Assets/Scripts/RubedoScripts/ArrowController.cs
+++ Assets/Scripts/RubedoScripts/ArrowController.cs
@@ -1,37 +1,72 @@
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.EventSystems;
+using System.Collections;
 
-public class ArrowController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+
+public class ArrowController : MonoBehaviour
 {
-    public Transform arrow;
     public Transform redRectangle;
     public Transform progressBar;
-    public Button cauldronButton;
+
 
     public float arrowSpeed = 3f;
     public float progressSpeed = 0.5f;
 
     private bool isArrowMovingUp;
     private bool isProgressBarFilling;
+    private float currentProgress;
+    private Rigidbody2D arrowRigidbody;
+    private Vector3 originalArrowPosition;
+    public float fallSpeedMultiplier = 0.5f; // Adjust this value to control the falling speed
 
-    public void OnPointerDown(PointerEventData eventData)
+
+
+    void Start()
     {
-        Debug.Log("Button Pressed");
-        if (!isArrowMovingUp)
+        // Get the Rigidbody2D component of the arrow
+        arrowRigidbody = GetComponent<Rigidbody2D>();
+
+        // Set the arrow's gravity scale to zero initially
+        arrowRigidbody.gravityScale = 0f;
+
+        // Store the original position of the arrow
+        originalArrowPosition = transform.localPosition;
+
+        // Get the Button component attached to the CauldronBtn GameObject
+        Button cauldronButton = GameObject.Find("CauldronBtn").GetComponent<Button>();
+
+        if (cauldronButton == null)
         {
-            isArrowMovingUp = true;
+            Debug.LogError("CauldronBtn button not found.");
+            return;
         }
+
+        // Add listeners to the button click events
+        cauldronButton.onClick.AddListener(OnCauldronButtonDown);
+        cauldronButton.onClick.AddListener(OnCauldronButtonUp);
     }
 
-    public void OnPointerUp(PointerEventData eventData)
+
+    public void OnCauldronButtonDown()
     {
-        Debug.Log("Button Released");
-        // Stop arrow movement when the button is released
-        isArrowMovingUp = false;
+        isArrowMovingUp = true;
+        // Set the arrow's velocity to move upwards when the button is clicked
+        arrowRigidbody.velocity = new Vector2(0, arrowSpeed);
+        // Enable gravity when the button is clicked
+        arrowRigidbody.gravityScale = 1f;
     }
 
-    private void Update()
+    public void OnCauldronButtonUp()
+    {
+        // Indicate that the arrow should stop moving up
+        isArrowMovingUp = false;
+
+        // Set the arrow's velocity to zero to stop its upward movement
+        arrowRigidbody.velocity = Vector2.zero;
+        arrowRigidbody.gravityScale = 85f;
+    }
+
+    void Update()
     {
         if (isArrowMovingUp)
         {
@@ -42,40 +77,69 @@ public class ArrowController : MonoBehaviour, IPointerDownHandler, IPointerUpHan
         {
             FillProgressBar();
         }
+        else
+        {
+            DecreaseProgressBar();
+        }
     }
 
-    private void MoveArrow()
+    void MoveArrow()
     {
-        Vector3 arrowPosition = arrow.localPosition;
+        // Get the current position of the arrow
+    Vector3 arrowPosition = transform.position;
 
-        arrowPosition.y += arrowSpeed * Time.deltaTime;
+    // Calculate the new position based on arrow speed and time
+    arrowPosition.y += arrowSpeed * Time.deltaTime;
 
-        if (arrowPosition.y >= redRectangle.localPosition.y)
-        {
-            isProgressBarFilling = true;
-        }
-        else if (arrowPosition.y <= -2f) // Original position of the arrow
-        {
-            isProgressBarFilling = false;
-        }
+    // Check if the arrow and the red rectangle are intersecting
+    bool isIntersecting = arrowPosition.y >= redRectangle.position.y;
 
-        arrow.localPosition = arrowPosition;
+    // Update the progress bar filling status
+    isProgressBarFilling = isIntersecting;
+
+    // Update the position of the arrow
+    transform.position = arrowPosition;
     }
 
-    private void FillProgressBar()
+
+    void FillProgressBar()
     {
-        Vector3 progressScale = progressBar.localScale;
-        progressScale.y += progressSpeed * Time.deltaTime;
+        // Get the maximum height of the progress bar
+        float maxProgressY = progressBar.GetComponent<RectTransform>().rect.height;
 
-        float maxProgressY = progressBar.GetComponent<SpriteRenderer>().bounds.size.y / 2f;
-        float progressY = Mathf.Clamp(progressScale.y, 0f, maxProgressY);
+        // Increase progress gradually
+        currentProgress += progressSpeed * Time.deltaTime;
+        currentProgress = Mathf.Clamp(currentProgress, 0f, maxProgressY);
 
-        progressBar.localScale = new Vector3(progressBar.localScale.x, progressY, progressBar.localScale.z);
+        // Update the scale of the progress bar
+        progressBar.localScale = new Vector3(progressBar.localScale.x, currentProgress, progressBar.localScale.z);
 
-        if (progressY >= maxProgressY)
+        if (currentProgress >= maxProgressY)
         {
             Debug.Log("You win!");
             // Implement win condition
         }
+    }
+
+    void DecreaseProgressBar()
+    {
+        // Decrease progress gradually
+        currentProgress -= progressSpeed * Time.deltaTime;
+        currentProgress = Mathf.Clamp(currentProgress, 0f, float.MaxValue);
+
+        // Update the scale of the progress bar
+        progressBar.localScale = new Vector3(progressBar.localScale.x, currentProgress, progressBar.localScale.z);
+    }
+
+    // Implement the logic to start arrow movement
+    public void StartArrowMovement()
+    {
+        isArrowMovingUp = true;
+    }
+
+    // Implement the logic to stop arrow movement
+    public void StopArrowMovement()
+    {
+        isArrowMovingUp = false;
     }
 }
